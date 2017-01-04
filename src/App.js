57d5b999dc0fa3ae401ef13fa698574d45420457
meng.fanjun <xmeng.fanjun@contract.isid.co.jp>
2017-01-04 10:43:15 +0900

    add router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter, Match, Link } from 'react-router'
+
+class Home extends Component {
+  render() {
+    return(
+      <h1>Home</h1>
+    )
+  }
+}
+
+class About extends Component {
+  render() {
+    return(
+      <h1>About</h1>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+          <Match exactly pattern="/" component={Home} />
+          <Match exactly pattern="/about" component={About} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      </BrowserRouter>
+    )
   }
 }
 
-export default App;
+export default App
